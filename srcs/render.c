e173fec4d93136f4587d924f98e14d8d1adf2d26
Alain <alain.sn.66@gmail.com>
2025-02-11 11:18:19 +0100

    distance int -> double

--- srcs/render.c
+++ srcs/render.c
@@ -6,18 +6,18 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:10:42 by asene             #+#    #+#             */
-/*   Updated: 2025/02/10 16:24:09 by asene            ###   ########.fr       */
+/*   Updated: 2025/02/11 11:17:26 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-double	segmentSize(int distance)
+double	segmentSize(double distance)
 {
 	return ((double)CELL_SIZE / (double)distance) * (double)W_WIDTH / (2 * tan(1.309 / 2));
 }
 
-int	correctedDistance(t_vars *vars, t_dpoint point, double rayAngle)
+double	correctedDistance(t_vars *vars, t_dpoint point, double rayAngle)
 {
 	double distance;
 
@@ -29,14 +29,14 @@ void draw_walls(t_vars *vars)
 {
 	int	i;
 	const int rayCount = W_WIDTH;
-	double ang = (75 * 3.14 / 180) / (rayCount - 1);
+	double ang = (75 * PI / 180) / (rayCount - 1);
 	double ray_angle;
 	i = 0;
 	while (i < rayCount)
 	{
-		ray_angle = vars->player->angle - (75 * 3.14 / 180) / 2 + ang * i;
+		ray_angle = vars->player->angle - (75 * PI / 180) / 2 + ang * i;
 		t_dpoint p = cast_ray(vars->map, vars->player->pos, ray_angle);
-		int	distance = correctedDistance(vars, p, ray_angle);
+		double	distance = correctedDistance(vars, p, ray_angle);
 		int size = segmentSize(distance);
 		draw_vline(vars->buffer, (t_point){i, (W_HEIGHT - size) / 2}, size, 0xFFFFF);
 		i++;
