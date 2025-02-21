69e30dc51f6710813e546bb9eb65caf318ec8905
Alain <alain.sn.66@gmail.com>
2025-02-21 17:07:01 +0100

    fix draw wall

--- srcs/render/render_walls.c
+++ srcs/render/render_walls.c
@@ -6,7 +6,7 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:10:42 by asene             #+#    #+#             */
-/*   Updated: 2025/02/16 09:52:05 by asene            ###   ########.fr       */
+/*   Updated: 2025/02/21 17:05:02 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,26 @@ int	get_pixel_scale(t_img *img, int x, int y, int size)
 
 void	draw_wall_segment(t_vars *vars, int ray_index, t_hit hit, int size)
 {
+	int	i;
 	int	j;
 	int	color;
 
-	j = 0;
-	while (j < size)
+	if (size <= W_HEIGHT)
 	{
-		color = get_pixel_scale(vars->textures[hit.side], hit.col_index, j, size);
+		i = 0;
+		j = size;
+	}
+	else
+	{
+		i = (size - W_HEIGHT) / 2;
+		j = W_HEIGHT + i;
+	}
+	while (i < j)
+	{
+		color = get_pixel_scale(vars->textures[hit.side], hit.col_index, i, size);
 		color =  color_shadowing(color, hit.distance);
-		put_pixel(vars->buffer, ray_index, j + (W_HEIGHT - size) / 2, color);
-		j++;
+		put_pixel(vars->buffer, ray_index, i + (W_HEIGHT - size) / 2, color);
+		i++;
 	}
 }
 
